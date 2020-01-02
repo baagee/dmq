f3024149588fee200a632dae2c1f836c01d82091
党留辉 <liuhuidang@sf-express.com>
2020-01-02 14:09:35 +0800

    去掉不需要的代码

--- client/main.go
+++ client/main.go
@@ -3,17 +3,6 @@ package main
 import "github.com/baagee/dmq/common"
 
 func main() {
-	//// 设置log 记录时间 文件和行号
-	//log.SetFlags(log.LstdFlags | log.Llongfile)
-	//// 设置中间件
-	//m := Middleware{}
-	//mm := alice.New(m.LogHandler, m.RecoverHandler, m.CheckParamsHandler, m.CheckProductHandler)
-	//// 设置路由
-	//http.Handle("/api/message/single", mm.ThenFunc(handler.Single))
-	//http.Handle("/api/message/batch", mm.ThenFunc(handler.Batch))
-	//
-	//log.Printf("http://127.0.0.1:%d\n", common.Config.HttpPort)
-	//http.ListenAndServe(fmt.Sprintf(":%d", common.Config.HttpPort), nil)
 	var app App
 	app.Init().BindRouter().Run(common.Config.HttpPort)
 }
